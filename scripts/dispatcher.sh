ec55199d6a701318b50b63ec4efdf1ec2f3aaeec
Doug Reed <dougr33d@gmail.com>
2024-07-07 13:02:56 -0500

    dispatch

--- scripts/dispatcher.sh
+++ scripts/dispatcher.sh
@@ -4,7 +4,13 @@
 # DISPATCHER
 #
 # This is a wrapper that executes python scripts with the appropriate python
-# build.  Run with `--DISPATCH-DEBUG` to see what exactly it's doing.
+# build.  
+#
+# Useful debugging args:
+# 
+#   --DISPATCH-DEBUG to see what exactly it's doing.
+#   --DISPATCH-FIND  to see what target python script is getting invoked
+# 
 
 NAME=$(basename $0)
 DISPATCH_DIR=$(dirname $(readlink -f $0))
@@ -12,6 +18,7 @@ DISPATCH_DIR=$(dirname $(readlink -f $0))
 PYTHON3="${DISPATCH_DIR}/venv/bin/python3"
 TARGET_PY="${DISPATCH_DIR}/${NAME}.py"
 DRY_RUN=0
+FIND_ONLY=0
 
 PASS_THROUGH_ARGS=()
 
@@ -19,6 +26,10 @@ while [[ $# -gt 0 ]]; do
     case $1 in
         --DISPATCH-DEBUG)
             DRY_RUN=1
+            ;;
+        --DISPATCH-FIND)
+            DRY_RUN=1
+            FIND_ONLY=1
             ;;
         *)
             PASS_THROUGH_ARGS+=("$1")
@@ -32,7 +43,7 @@ if [[ -e "${TARGET_PY}" ]]; then
     if [[ "$DRY_RUN" -eq 0 ]]; then
         COMMAND="${PYTHON3} ${TARGET_PY} ${PASS_THROUGH_ARGS[@]}"
         exec ${COMMAND}
-    else
+    elif [[ "$FIND_ONLY" -eq 0 ]]; then
         ME=$(readlink -f $0)
         echo "Hello from dispatch!"
         echo
@@ -44,5 +55,7 @@ if [[ -e "${TARGET_PY}" ]]; then
         echo "Without --DISPATCH-DEBUG, I would have exec'd:"
         echo
         echo "   $ ${COMMAND}"
+    else
+        echo "${TARGET_PY}"
     fi
 fi
