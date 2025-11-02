08285126221ff0e9641a7d70cc56a614ee56e222
Doug Reed <dougr33d@gmail.com>
2025-11-02 12:31:20 -0600

    more neovim

--- setup_env/nvim/lua/dougr33d_bootstrap/set.lua
+++ setup_env/nvim/lua/dougr33d_bootstrap/set.lua
@@ -5,3 +5,7 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
+-- Abolish misspelling corrections
+vim.cmd("Abolish propogat{e,ion} propagat{}")
+vim.cmd("Abolish gerp grep")
+vim.cmd("Abolish teh the")
